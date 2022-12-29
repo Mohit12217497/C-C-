81fc92ed721936a3bbdf3de032e3a93170d8b2c3
Mohit12217497 <119718639+Mohit12217497@users.noreply.github.com>
2022-12-29 12:48:40 +0530

    Add files via upload

---
+++ operators (unary).cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{int x=3;
+x++;
+printf("%d",x);
+++x;
+printf("%d",x);
+x++;
+printf("%d",x);
+x--;
+printf("%d",x);
+--x;
+printf("%d",x);
+x++;
+printf("%d",x);
+}
